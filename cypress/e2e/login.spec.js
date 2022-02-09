8143d5343583f4ed4fc880b356f70e29a5902d3c
acavalls <alejandro.canosa@bsc.es>
2022-02-09 11:22:40 +0100

    Test env: Adding Cypress conf & initial test

---
+++ cypress/e2e/login.spec.js
@@ -0,0 +1,14 @@
+import "cypress-localstorage-commands";
+
+describe("Stories: Login - A user fill in the login form, and it is able to navigate through the portal with the proper role.", () => {
+    beforeEach( () => {
+      cy.visit('/');
+    });
+    afterEach( () => {
+      cy.logout(Cypress.env("logoutUrl"));
+    });
+    it("Login and check this is a dac-admin user", () => {
+        cy.login(Cypress.env("adminUsername"), Cypress.env("adminUserPassword"));
+        cy.getLocalStorage("role").should("equal", "dac-admin");
+    })
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,15 @@
+Cypress.Commands.add('getStore', () => { 
+  cy.window().its('store');
+});
+
+Cypress.Commands.add('login', (username, pwd) => { 
+  cy.get("#username").type(username);
+  cy.get("#password").type(pwd);
+  cy.get("#kc-login").click();
+  cy.visit("/");
+  cy.wait(4000);
+});
+
+Cypress.Commands.add('logout', (url) => {
+  cy.window().then(win => win.location.href = url);
+});
