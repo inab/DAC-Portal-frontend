de2414438406ed22b3179e36a60f7ca8564f1ef8
acavalls <alejandro.canosa@bsc.es>
2022-03-01 18:48:16 +0100

    Adding requests/authZ validators & refactoring

--- server/routes/dac.js
+++ server/routes/dac.js
@@ -1,29 +1,29 @@
 import { Router } from 'express';
 import jwt_decode from "jwt-decode";
 import { getUserRequests, getDacData, updatePolicies, getUserDacs } from '../services/dacmember';
+import resource from '../middleware/authZ';
+import request from '../middleware/request';
 
 export default ({ keycloak }) => {
 	let api = Router();
 
-	api.get('/data', keycloak.protect('is-dac'), async function(req, res){
+        api.get('/data', [keycloak.protect(), request, resource], async function(req, res){
 		const userInfo = jwt_decode(req.headers.authorization)
                 const response = await getDacData(userInfo.sub);
 		res.send(response)
         })
-        api.get('/requests', keycloak.protect('is-dac'), async function(req,res) {
+        api.get('/requests', [keycloak.protect(), request, resource], async function(req,res) {
                 const userInfo = jwt_decode(req.headers.authorization)
                 const userDacs = await getUserDacs(userInfo.sub);
                 const response = await Promise.all(
-			userDacs.map(async (item) => await getUserRequests(item.dacId))
+		    userDacs.map(async (item) => await getUserRequests(item.dacId))
 		)               
 		res.send([].concat(...response))
         })
-        api.put('/policies', keycloak.protect('dac-admin'), async function(req,res) {
+        api.put('/policies', [keycloak.protect(), request, resource], async function(req,res) {
                 const userInfo = jwt_decode(req.headers.authorization)
-                const dacId = req.param('dac-id');
-                const fileId = req.param('ds-id');
-                const policy = req.param('policy');
-                const response = await updatePolicies(userInfo.sub, dacId, fileId, policy);
+                // const acl = req.param('acl');
+                const response = await updatePolicies(userInfo.sub, req.param('dac-id'), req.param('ds-id'), req.param('policy'));
 		res.send(response)
         })
 
