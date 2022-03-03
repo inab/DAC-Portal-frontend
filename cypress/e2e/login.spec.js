1a5b508b8ed0b5f06e0af3de2d1531493c98b46d
acavalls <alejandro.canosa@bsc.es>
2022-03-03 13:49:52 +0100

    Cypress (e2e): Updating login tests

--- cypress/e2e/login.spec.js
+++ cypress/e2e/login.spec.js
@@ -11,4 +11,8 @@ describe("Stories: Login - A user fill in the login form, and it is able to navi
         cy.login(Cypress.env("adminUsername"), Cypress.env("adminUserPassword"));
         cy.getLocalStorage("role").should("equal", "dac-admin");
     })
+    it("Login and check this is a dac-member user", () => {
+      cy.login(Cypress.env("memberUsername"), Cypress.env("memberUserPassword"));
+      cy.getLocalStorage("role").should("equal", "dac-member");
+  })
 });
