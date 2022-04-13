af10bccf2c6d024c9cc53559b28383d8f0425620
acavalls <alejandro.canosa@bsc.es>
2022-04-13 13:09:48 +0200

    Assing roles based on dac:roles scope

--- src/index.js
+++ src/index.js
@@ -7,48 +7,50 @@ const Keycloak = require('keycloak-js');
 const { REACT_APP_AUTH_URL, REACT_APP_AUTH_REALM, REACT_APP_AUTH_CLIENT } = process.env
 
 let initOptions = {
-  url: REACT_APP_AUTH_URL, realm: REACT_APP_AUTH_REALM, clientId: REACT_APP_AUTH_CLIENT, onLoad: 'login-required'
+    url: REACT_APP_AUTH_URL, realm: REACT_APP_AUTH_REALM, clientId: REACT_APP_AUTH_CLIENT, onLoad: 'login-required'
 }
 
 let keycloak = Keycloak(initOptions);
 
 keycloak.init({ onLoad: initOptions.onLoad }).success((auth) => {
 
-  if (!auth) {
-      window.location.reload();
-  } else {
-      console.info("Authenticated");
-  }
+    if (!auth) {
+        window.location.reload();
+    } else {
+        console.info("Authenticated");
+    }
 
-  console.log(keycloak["tokenParsed"]["realm_access"])
-  console.log(keycloak.token)
+    localStorage.setItem("react-token", keycloak.token);
+    localStorage.setItem("react-refresh-token", keycloak.refreshToken);
 
-  localStorage.setItem("react-token", keycloak.token);
-  localStorage.setItem("react-refresh-token", keycloak.refreshToken);
+    let roles;
 
-    if(keycloak["tokenParsed"]["realm_access"]["roles"].includes("app-dac-admin")){
-        localStorage.setItem("role", 'dac-admin');
-    } else if (keycloak["tokenParsed"]["realm_access"]["roles"].includes("app-dac-member")){
-        localStorage.setItem("role", 'dac-member');
+    if (keycloak.tokenParsed["dac:roles"]) {
+        roles = keycloak.tokenParsed["dac:roles"].filter(n => n)[0];
+        if (roles.map(el => el.includes("dac-admin"))) {
+            localStorage.setItem("role", 'dac-admin');
+        } else if (roles.map(el => elincludes("dac-member"))) {
+            localStorage.setItem("role", 'dac-member');
+        }
     } else {
         localStorage.setItem("role", 'user');
     }
- 
-  setTimeout(() => {
-      keycloak.updateToken(70).success((refreshed) => {
-          if (refreshed) {
-              console.debug('Token refreshed' + refreshed);
-          } else {
-              console.warn('Token not refreshed, valid for '
-                  + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
-          }
-      }).error(() => {
-          console.error('Failed to refresh token');
-      });
-  }, 60000)
 
-  ReactDOM.render(<App />, document.getElementById('root'));
+    setTimeout(() => {
+        keycloak.updateToken(70).success((refreshed) => {
+            if (refreshed) {
+                console.debug('Token refreshed' + refreshed);
+            } else {
+                console.warn('Token not refreshed, valid for '
+                    + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+            }
+        }).error(() => {
+            console.error('Failed to refresh token');
+        });
+    }, 60000)
+
+    ReactDOM.render(<App />, document.getElementById('root'));
 
 }).error(() => {
-  console.error("Authenticated Failed");
+    console.error("Authenticated Failed");
 });
