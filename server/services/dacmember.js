b8fbeead80648cb458cadef705e3b28a76099f92
acavalls <alejandro.canosa@bsc.es>
2022-04-26 14:02:40 +0200

    Services (DAC-admin): Adding method for handling form data

--- server/services/dacmember.js
+++ server/services/dacmember.js
@@ -9,8 +9,8 @@ import { DacData } from '../models/dac-data';
 // 1.a. GET USER REQUESTS ASSIGNED TO THIS DAC-MEMBER BY ID -> EVENTUALLY, THIS QUERY WILL USE DAC-ID INSTEAD (WE DON'T HAVE DAC IDs YET) 
 
 const getUserRequests = async (id) => {
-    const response = await DacRequests.find({ 'dacId' : id })
-                                      .select({ 'requests' : 1, 'dacId': 1, '_id' : 0});
+    const response = await DacRequests.find({ 'dacId': id })
+                                      .select({ 'requests': 1, 'dacId': 1, '_id': 0 });
 
     return response
 }
@@ -18,17 +18,16 @@ const getUserRequests = async (id) => {
 // 1.a. GET USER REQUESTS ASSIGNED TO THIS DAC-MEMBER BY ID
 
 const getUserDacs = async (id) => {
-    const response = await DacData.find({ 'members' : id })
-                                  .select({ '_id' : 0, 'members' : 0, 'files' : 0 });
+    const response = await DacData.find({ 'members': id })
+                                  .select({ '_id': 0, 'members': 0, 'files': 0 });
     return response
 }
 
 // 1.b. GET DAC DATA: DAC Data associated to a specific user.
 
 const getDacData = async (id) => {
-    const response = await DacData.find({ 'members' : id })
-                                  .select({'_id' : 0, 'members': 0 });
-
+    const response = await DacData.find({ 'members': id })
+                                  .select({ '_id': 0, 'members': 0 });
     return response
 }
 
@@ -36,30 +35,59 @@ const getDacData = async (id) => {
 
 const updatePolicies = async (id, dac, file, policy) => {
 
-    let resourceData = await DacData.find({ 'dacId' : dac, 'files.fileId' : file })
-                                    .select({'_id' : 0, 'files.$' : 1 });
+    let resourceData = await DacData.find({ 'dacId': dac, 'files.fileId': file })
+                                    .select({ '_id': 0, 'files.$': 1 });
 
     let { acl } = resourceData[0].files[0];
 
-    let obj = {
-        'fileId' : file,
-        'policy' : policy,
-        'acl'    : acl  
-    }
-    
-    let response = await DacData.findOneAndUpdate({ 'dacId' : dac, 'files.fileId' : file },
-                                { $set : { 'files.$' : obj }});
+    let obj = { 'fileId': file,
+                'policy': policy,
+                'acl': acl }
+
+    let response = await DacData.findOneAndUpdate({ 'dacId': dac, 'files.fileId': file },
+                                                  { $set: { 'files.$': obj } });
 
     response = await getDacData(id);
-    
+
     return response
 }
 
 // 1.d. GET POLICIES BY DAC-ID AND FILE-ID
 
 const getPolicies = async (dac, file) => {
-    const response = await DacData.find({ 'dacId' : dac, 'files.fileId' : file })
-                                  .select({'_id' : 0, 'members' : 0, 'files.$' : 1 });
+    const response = await DacData.find({ 'dacId': dac, 'files.fileId': file })
+                                  .select({ '_id': 0, 'members': 0, 'files.$': 1 });
+
+    return response
+}
+
+// 1.e. Update DAC info: 
+
+const updateDacInfo = async (id, info) => {
+
+    let { dacId,
+          dacName,
+          dacStudy,
+          datasets,
+          adminName,
+          adminSurname,
+          emailAddress,
+          studyDescription } = info
+
+    let obj = { 'dacName': dacName,
+                'dacStudy': dacStudy,
+                'datasets': datasets,
+                'adminName': adminName,
+                'adminSurname': adminSurname,
+                'emailAddress': emailAddress,
+                'studyDescription': studyDescription,
+                'status': false }
+
+    await DacData.findOneAndUpdate({ 'dacId': dacId },
+                                   { 'info': obj },
+                                   { new: true });
+
+    let response = await getDacData(id);
 
     return response
 }
@@ -67,5 +95,6 @@ const getPolicies = async (dac, file) => {
 exports.getUserRequests = getUserRequests;
 exports.getDacData = getDacData;
 exports.updatePolicies = updatePolicies;
+exports.updateDacInfo = updateDacInfo;
 exports.getPolicies = getPolicies;
 exports.getUserDacs = getUserDacs;
