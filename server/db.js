022518420a6dd987dfde2343125430cb17427dbb
acavalls <alejandro.canosa@bsc.es>
2022-07-04 16:16:20 +0200

    Production: Setting up a proper configuration (MongoDB, Dockerfile.production,etc)

--- server/db.js
+++ server/db.js
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose';
 import winston from 'winston';
-require('dotenv').config();
 
 export default callback => {
-	let host = process.env.NODE_ENV === 'test' ? process.env.MONGO_HOST_TEST : process.env.MONGO_HOST;	
-	let db = process.env.MONGO_DB;
-	let username = process.env.MONGO_USER;
-	let password = process.env.MONGO_PASS;
-	let authSource = process.env.MONGO_AUTH;
-	// connect to MongoDB, then pass it to the callback fn:
-	const mongo = mongoose.connect(`mongodb://${username}:${password}@${host}/${db}?connectTimeoutMS=300000&replicaSet=rs0&authSource=admin`)
-		.then(() => winston.info(`Connected to ${db}`))
-	callback(mongo); 
+        let host = process.env.NODE_ENV === 'prod' ? process.env.MONGO_HOST : process.env.MONGO_HOST_TEST;
+        let db = process.env.MONGO_DB;
+        let username = process.env.MONGO_USER;
+        let password = process.env.MONGO_PASS;
+        let authSource = process.env.MONGO_AUTH;
+        // connect to MongoDB, then pass it to the callback fn:
+        let uri = process.env.NODE_ENV === 'prod' ? `mongodb://${username}:${password}@${host}/${db}?authSource=${authSource}` :
+            `mongodb://${username}:${password}@${host}/${db}?connectTimeoutMS=300000&replicaSet=rs0&authSource=${authSource}`
+        const mongo = mongoose.connect(uri)
+            .then(() => winston.info(`Connected to ${db}`))
+        callback(mongo);
 }
