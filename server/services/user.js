8d564e661cb821d087cdc6624b46283c2e5b4603
acavalls <alejandro.canosa@bsc.es>
2022-07-06 19:01:32 +0200

    User request (services): Adding Mongoose (MongoDB) transaction (ACID) for multiple collections

--- server/services/user.js
+++ server/services/user.js
@@ -1,4 +1,5 @@
 // IMPORT MONGOOSE MODELS.
+import mongoose from 'mongoose';
 import { User } from '../models/user';
 import { DacRequests } from '../models/dac-requests';
 import { DacData } from '../models/dac-data';
@@ -32,17 +33,42 @@ const getRequestedFileData = async (file) => {
 }
 
 // 1.C BUILD REQUEST OBJECT FROM FILE INFORMATION AND USER REQUEST METADATA
-const buildRequestObject = (file, resource, comments, status) => {
+const buildRequestObject = (file, resource, comments) => {
     return {
         fileId: file,
         resource: resource,
-        comment: comments,
-        status: status
+        comment: comments
     }
 }
 
-// 1.D. POST A USER REQUEST FOR A SPECIFIC DAC
-const postRequest = async (id, dacId, request) => {
+// 1.D CREATE A TRANSACTION FOR STORING USER REQUEST DATA (collections: user (USER-VIEW) - dac-requests (DAC VIEWS))
+const requestTransaction = async (id, dacId, request) => {
+    const session = await mongoose.startSession();
+
+    let isCompleted = false;
+
+    try {
+        session.startTransaction();
+        await setUserRequestData(id, dacId, request, session)
+        await setUserRequestStatus(id, request, session)
+        await session.commitTransaction();
+    } catch (e) {
+        await session.abortTransaction();
+        return { response: false };
+    } finally {
+        if (session.transaction.state === "TRANSACTION_COMMITTED") {
+            isCompleted = true;
+        } else {
+            isCompleted = false;
+        }
+        await session.endSession();
+        return { response: isCompleted };
+    }
+}
+
+
+// 1.E SET USER REQUEST DATA (DAC VIEW)
+const setUserRequestData = async (id, dacId, request, session) => {
 
     let userReq = { 'user': id, 'requests': [request] }
 
@@ -57,22 +83,47 @@ const postRequest = async (id, dacId, request) => {
     let response = [];
 
     if(dac.length === 0) {
-        response = await DacRequests.create({ 'dacId' : dacReq.dacId, 'requests' : dacReq.requests });
+        response = await DacRequests.create([{ 'dacId' : dacReq.dacId, 'requests' : dacReq.requests }], { session });
     } else if(user.length === 0) {
         response = await DacRequests.findOneAndUpdate(
             { 'dacId' : dacId },
             { $addToSet : { 'requests' : userReq } },
-            { new: true });
+            { new: true, session });
     } else {
         response = await DacRequests.findOneAndUpdate(
             { 'dacId' : dacId, 'requests.user' : id },
             { $addToSet : { "requests.$.requests" : request } },
-            { new: true }); 
+            { new: true, session }); 
     } 
 
     return response
 }
-// 1.E. GET REQUESTS STATUS BY USER ID
+
+// 1.F SET USER REQUEST STATUS (USER VIEW)
+const setUserRequestStatus = async (id, request, session) => {
+    let fileStatusObject = {
+        fileId: request.fileId,
+        status: "Pending"
+    }
+    let user = await User.find({ 'sub' : id })
+                         .select({'_id' : 0 });
+
+    let response;
+
+    if(user.length === 0){
+        response = await User.create([{ 'sub': id, 'status': fileStatusObject }], { session });
+    } else {
+        response = await User.findOneAndUpdate(
+            { 'sub': id },
+            { $addToSet: { "status" : fileStatusObject }},
+            { new: true, session }
+        )
+    }
+
+    return response
+}
+
+// 1.G. GET REQUESTS STATUS BY USER ID
 const getRequestsStatus = async (id) => {
     const response = await User.find({ 'sub' : id })
                                .select({ 'status' : 1, '_id' : 0});
@@ -80,7 +131,7 @@ const getRequestsStatus = async (id) => {
 }  
 
 exports.getPolicies = getPolicies;
-exports.postRequest = postRequest;
 exports.getRequestsStatus = getRequestsStatus;
 exports.getRequestedFileData = getRequestedFileData;
 exports.buildRequestObject = buildRequestObject;
+exports.requestTransaction = requestTransaction;
