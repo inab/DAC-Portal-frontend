9d0f3eaf24bef8cf8b030cae85235523db199e8a
acavalls <alejandro.canosa@bsc.es>
2022-07-06 16:01:24 +0200

    Cleaning code: Replacing callbacks with promises when initialising DB (Mongoose)

--- server/db.js
+++ server/db.js
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose';
-import winston from 'winston';
 
-export default callback => {
-        let host = process.env.NODE_ENV === 'prod' ? process.env.MONGO_HOST : process.env.MONGO_HOST_TEST;
-        let db = process.env.MONGO_DB;
-        let username = process.env.MONGO_USER;
-        let password = process.env.MONGO_PASS;
-        let authSource = process.env.MONGO_AUTH;
-        // connect to MongoDB, then pass it to the callback fn:
-        let uri = process.env.NODE_ENV === 'prod' ? `mongodb://${username}:${password}@${host}/${db}?authSource=${authSource}` :
-            `mongodb://${username}:${password}@${host}/${db}?connectTimeoutMS=300000&replicaSet=rs0&authSource=${authSource}`
-        const mongo = mongoose.connect(uri)
-            .then(() => winston.info(`Connected to ${db}`))
-        callback(mongo);
-}
+export default async () => {
+    let host = process.env.NODE_ENV === 'prod' ? process.env.MONGO_HOST : process.env.MONGO_HOST_TEST;
+    let db = process.env.MONGO_DB;
+    let username = process.env.MONGO_USER;
+    let password = process.env.MONGO_PASS;
+    let authSource = process.env.MONGO_AUTH;
+    let uri = process.env.NODE_ENV === 'prod' ? `mongodb://${username}:${password}@${host}/${db}?authSource=${authSource}` :
+        `mongodb://${username}:${password}@${host}/${db}?connectTimeoutMS=300000&replicaSet=rs0&authSource=${authSource}`
+    try {
+      await mongoose.connect(uri);
+      console.log("Connected to the database: ", db);
+    } catch (e) {
+      console.error("Database connection failed:", e);
+    }
+};
--- server/index.js
+++ server/index.js
@@ -3,15 +3,16 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
-import initDb from './db';
-import winston from 'winston';
 import userRoutes from './routes/user';
 import dacRoutes from './routes/dac';
 import { keycloak, sessionData, serverConf } from './config';
 import 'express-async-errors';
 import errors from './middleware/errors';
+import initDb from './db';
 require('dotenv').config();
 require('./logs')();
+
+initDb();
 
 let app = express();
 
@@ -24,7 +25,7 @@ app.use(cors({
 }));
 
 app.use(bodyParser.json({
-	limit : serverConf.bodyLimit
+	limit: serverConf.bodyLimit
 }));
 
 app.use(sessionData);
@@ -33,17 +34,14 @@ app.use(keycloak.middleware());
 
 app.set('trust proxy', true);
 
-initDb( db => {
+app.use('/user', userRoutes({ keycloak }));
 
-	app.use('/user', userRoutes({ keycloak }));
+app.use('/dac', dacRoutes({ keycloak }));
 
-	app.use('/dac', dacRoutes({ keycloak }));
+app.use(errors)
 
-	app.use(errors)
-
-	app.server.listen(process.env.PORT_API || serverConf.port, () => {
-		console.log(`Started on port ${app.server.address().port}`);
-	});
+app.server.listen(process.env.PORT_API || serverConf.port, () => {
+	console.log(`Started on port ${app.server.address().port}`);
 });
 
 export default app;
