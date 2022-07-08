c65cc249b780b1a6fc16691cc5bd468e23ac297f
acavalls <alejandro.canosa@bsc.es>
2022-07-08 12:05:23 +0200

    Pages (ManagePermissions): Cleaning code (State simplification, Labels, Table rendering, etc)

--- src/pages/ManagePermissions.js
+++ src/pages/ManagePermissions.js
@@ -3,92 +3,103 @@ import { Card, Container, Row, Col, Table, Button } from "react-bootstrap";
 import { getUsersPermissions, deleteUserPermissions } from '../Services/ManagePermissions';
 
 const { REACT_APP_DAC_PORTAL_API_URL, REACT_APP_PERMISSIONS_URL } = process.env
- 
+
+const PAGE_LABELS = {
+  title: "Manage permissions",
+  subtitle: "Here you can revoke permissions which are related with your DACs",
+  empty: "No permissions have been assigned on datasets controlled by your DAC"
+};
+
+const TABLE_LABELS = {
+  sub: "User",
+  type: "Type",
+  value: "Value (resource)",
+  source: "DAC",
+  by: "BY",
+  asserted: "Timestamp",
+  revoke: "Revoke"
+}
+
 const ManagePermissions = () => {
 
-  const [request, setRequest] = useState({ type: 'get',
-                                           url: `${REACT_APP_DAC_PORTAL_API_URL}/dac/requests`, 
-                                           token: localStorage.getItem("react-token"),
-                                           params: {
-                                               'format' : null,
-                                               'account-id': null
-                                           } });
-                
+  const [request, setRequest] = useState({
+    type: 'get',
+    url: `${REACT_APP_DAC_PORTAL_API_URL}/dac/requests`,
+    token: localStorage.getItem("react-token"),
+    params: {
+      'format': null,
+      'account-id': null
+    }
+  });
+
   const [items, setItems] = useState([]);
 
-  const [mainTitles, setMainTitles] = useState({ title: "Manage permissions", 
-                                                 subtitle: "Here you can revoke permissions which are related with your DACs"})
-  const [cardTitles, setCardTitles] = useState(["User", "Type", "Timestamp", "Value (Resource)", "DAC", "BY", "Revoke"])
-  
   useEffect(() => {
     (async () => {
-      try { 
-        request.type === "get" ? setItems(await getUsersPermissions(request)) : 
+      try {
+        request.type === "get" ? setItems(await getUsersPermissions(request)) :
                                  setItems(await deleteUserPermissions(request, items))
-      } catch (err) { 
-        console.log("error ", err.message) 
+      } catch (err) {
+        console.log("error ", err.message)
         alert("An error ocurred: Users permissions assigned by your DACs could not be loaded")
       }
     })();
   }, [request]);
 
-  const handlePermissions = async (e, d, idx) => {
+  const handlePermissions = async (e, object, idx) => {
     e.preventDefault();
-    setRequest({  type: 'delete', 
-                  url: `${REACT_APP_PERMISSIONS_URL}/permissions`, 
-                  token: localStorage.getItem("react-token"),
-                  params: {
-                    'values' : `${d.ga4gh_visa_v1.value}`,
-                    'account-id': `${d.sub}` },
-                  index: idx }); 
+    setRequest({
+      type: 'delete',
+      url: `${REACT_APP_PERMISSIONS_URL}/permissions`,
+      token: localStorage.getItem("react-token"),
+      params: {
+        'values': `${object.ga4gh_visa_v1.value}`,
+        'account-id': `${object.sub}`
+      },
+      index: idx
+    });
   }
- 
-  const Items = () => {
-    return items.map((d, idx) => {
-        return ( 
-            <tr>
-                <td> {d.sub} </td>
-                <td> {d.ga4gh_visa_v1.type} </td>
-                <td> {d.ga4gh_visa_v1.asserted} </td>
-                <td> {d.ga4gh_visa_v1.value} </td>
-                <td> {d.ga4gh_visa_v1.source} </td>
-                <td> {d.ga4gh_visa_v1.by} </td>
-                <td className="text-center">
-                    <Button variant="success" className="btn-block btn-fill" onClick={(e) => handlePermissions(e, d, idx)}>Revoke</Button>
-                </td>       
-            </tr>
-        )
-    })}
+
+  const TableRowData = (props) => {
+    const { row } = props;
+    return Object.values(row).map((value) => <td> {value} </td> )
+  }
 
   return (
     <Container fluid>
-      <h2> {mainTitles.title} </h2>
-      <h4> {mainTitles.subtitle} </h4>
+      <h2> {PAGE_LABELS.title} </h2>
+      <h4> {PAGE_LABELS.subtitle} </h4>
+
       {items.length >= 1 ? (
-      <Row>
-        <Col md="12">
+        <Row>
+          <Col md="12">
             <Card className="strpied-tabled-with-hover">
-                <Card.Body className="table-full-width table-responsive px-0">
-                    <Table className="table-hover table-striped">
-                        <thead>
-                            <tr>
-                                <th className="border-0">{cardTitles[0]}</th>
-                                <th className="border-0">{cardTitles[1]}</th>
-                                <th className="border-0">{cardTitles[2]}</th>
-                                <th className="border-0">{cardTitles[3]}</th>
-                                <th className="border-0">{cardTitles[4]}</th>
-                                <th className="border-0">{cardTitles[5]}</th>
-                                <th className="border-0">{cardTitles[6]}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                          <Items/>
-                        </tbody>
-                    </Table>
-                </Card.Body>
-            </Card>          
-        </Col>
-      </Row> ) : <p> No permissions have been assigned on datasets controlled by your DAC </p> }
+              <Card.Body className="table-full-width table-responsive px-0">
+                <Table className="table-hover table-striped">
+                  <thead>
+                    <tr>
+                      {Object.values(TABLE_LABELS).map(element => <th className="border-0"> {element} </th>)}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {items.map((object, idx) => {
+                      const { sub, ga4gh_visa_v1 } = object;
+                      return (
+                        <tr> 
+                          <TableRowData row={Object.assign({}, { 'sub': sub }, { ...ga4gh_visa_v1 })} />   
+                          <td className="text-center">
+                            <Button variant="success" className="btn-block btn-fill" 
+                                    onClick={(e) => handlePermissions(e, object, idx)}> Revoke 
+                            </Button>
+                          </td>   
+                        </tr>
+                    )})}
+                  </tbody>
+                </Table>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row> ) : <p> {PAGE_LABELS.empty} </p> }
     </Container>
   )
 }
