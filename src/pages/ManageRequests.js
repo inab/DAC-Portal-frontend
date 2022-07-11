95c300ad0a320c01b31fb0349a51ce86df133698
acavalls <alejandro.canosa@bsc.es>
2022-07-11 16:38:14 +0200

    Refactoring (III.b): Abstracting implementation details from ManageRequests page + cleaning code

--- src/pages/ManageRequests.js
+++ src/pages/ManageRequests.js
@@ -1,130 +1,91 @@
 import React, { useEffect, useState } from 'react';
-import { itemsDestructuring } from '../utils/utils';
-import axios from 'axios';
-import {
-  Card,
-  Container,
-  Row,
-  Col,
-  Table,
-  Button
-} from "react-bootstrap";
+import { Card, Container, Row, Col, Table, Button } from "react-bootstrap";
+import { getUsersRequests, postUserPermissions } from '../Services/ManageRequests';
+import useRequest from '../hooks/ManageRequestsReducer';
 
-const { REACT_APP_DAC_PORTAL_API_URL, REACT_APP_PERMISSIONS_URL } = process.env
+const PAGE_LABELS = {
+  title: "Manage requests",
+  subtitle: "Here you can grant/deny incoming Data Access Requests",
+  empty: "No requests have been made to your DAC"
+};
+
+const TABLE_LABELS = {
+  user: "User",
+  fileId: "File ID",
+  resource: "Resource",
+  comments: "Comments",
+  access: "Access"
+}
 
 const RequestsStatus = () => {
-
-  const [request, setRequest] = useState({ type: 'get',
-                                           url: `${REACT_APP_DAC_PORTAL_API_URL}/dac/requests`, 
-                                           token: localStorage.getItem("react-token"),
-                                           params: {
-                                            'format' : null,
-                                            'account-id': null
-                                           },
-                                           data: null });
-                
-  const [response, setResponse] = useState([]);
-
-  const [mainTitles, setMainTitles] = useState({ title: "Manage requests", 
-                                                 subtitle: "Here you can grant/deny incoming Data Access Requests"})
-  const [cardTitles, setCardTitles] = useState(["User", "File ID", "Resource", "Comments", "Access"])
+  const [request, dispatch] = useRequest();
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
     (async () => {
-      const query = await axios({ 
-        method: request.type, 
-        url: request.url, 
-        headers: {
-          Authorization: "Bearer " + request.token
-        },
-        params: request.params,
-        data: request.data
-      }).then(res => {
-          if(request.type === "get") {
-            res = [].concat(...res.data.map(item => item.requests))
-            return itemsDestructuring(res)
-          } else {
-            let removeItem = response.filter((el, idx) => idx !== request.index);
-            // This specific item should be removed in the dac-requests collection. As for now, only the UI will be updated upon permissions addition. 
-            return removeItem
-          }
-      }).catch(error => {
-      });
-      setResponse(query);
+      try {
+        request.type === "get" ? setItems(await getUsersRequests(request)) :
+                                 setItems(await postUserPermissions(request, items))
+      } catch (err) {
+        console.log("error ", err.message)
+        alert("An error ocurred: Users requests assigned to your DACs could not be loaded")
+      }
     })();
-    //apiRequest();
   }, [request]);
 
-  const handlePermissions = async (e, d, idx) => {
-    e.preventDefault();
-
+  const handlePermissions = async (e, object, index) => {
     let assertions = [{
-        type : "ControlledAccessGrants",
-        asserted: 1564814387,
-        value: `${d.resource}`,
-        source: "https://test-url/source_dac",
-        by: "dac"
-    }]   
+      type: "ControlledAccessGrants",
+      asserted: 1564814387,
+      value: `${object.resource}`,
+      source: "https://test-url/source_dac",
+      by: "dac"
+    }]
 
-    setRequest({ type: 'post', 
-                 url: `${REACT_APP_PERMISSIONS_URL}/permissions`, 
-                 token: localStorage.getItem("react-token"),
-                 data: assertions,
-                 params: {
-                     'format' : "PLAIN",
-                     'account-id' : `${d.user}`
-                 }, 
-                 index: idx
-    })
+    dispatch({ type: "post", payload: { assertions: assertions, user: object.user, index: index } })
   }
 
-  const ShowResponse = () => {
-    return response.map((d, idx) => {
-        return ( 
-          <tr>
-            <td> {d.user} </td>
-            <td> {d.fileId} </td>
-            <td> {d.resource} </td>
-            <td> {d.comment} </td>
-            <td className="text-center">
-              <Button variant="success" className="btn-block btn-fill" onClick={(e) => handlePermissions(e, d, idx)}>Grant</Button>
-              <Button variant="danger" className="btn-block btn-fill disabled" onClick={(e) => e.preventDefault()}>Deny</Button>
-            </td>
-          </tr>
-        )
-    })
+  const TableRowData = (props) => {
+    const { row } = props;
+    return Object.values(row).map((value) => <td> {value} </td>)
   }
 
   return (
     <>
-    <Container fluid>
-      {response.length >= 1 ? (
-        <Row>
+      <Container fluid>
+        {items.length >= 1 ? (
+          <Row>
             <Col md="12">
-                <h4> {mainTitles.title} </h4>
-                <p> {mainTitles.subtitle} </p>
-                <Card className="strpied-tabled-with-hover">
-                    <Card.Body className="table-full-width table-responsive px-0">
-                        <Table className="table-hover table-striped">
-                            <thead>
-                                <tr>
-                                    <th className="border-0">{cardTitles[0]}</th>
-                                    <th className="border-0">{cardTitles[1]}</th>
-                                    <th className="border-0">{cardTitles[2]}</th>
-                                    <th className="border-0">{cardTitles[3]}</th>
-                                    <th className="border-0">{cardTitles[4]}</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                              <ShowResponse/>
-                            </tbody>
-                        </Table>
-                    </Card.Body>
-                </Card>
+              <h2> {PAGE_LABELS.title} </h2>
+              <h4> {PAGE_LABELS.subtitle} </h4>
+              <Card className="strpied-tabled-with-hover">
+                <Card.Body className="table-full-width table-responsive px-0">
+                  <Table className="table-hover table-striped">
+                    <thead>
+                      <tr>
+                        {Object.values(TABLE_LABELS).map(element => <th className="border-0"> {element} </th>)}
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {items.map((object, index) => {
+                        return (
+                          <tr>
+                            <TableRowData row={object} />
+                            <td className="text-center">
+                              <Button variant="success" className="btn-block btn-fill" onClick={(e) => handlePermissions(e, object, index)}>Grant</Button>
+                              <Button variant="danger" className="btn-block btn-fill disabled" onClick={(e) => e.preventDefault()}>Deny</Button>
+                            </td>
+                          </tr>
+                        )
+                      })}
+                    </tbody>
+                  </Table>
+                </Card.Body>
+              </Card>
             </Col>
-      </Row> ) : <p> No requests have been made to your DAC </p> }
-    </Container>
-  </>
+          </Row>) : <p> {PAGE_LABELS.empty} </p>}
+      </Container>
+    </>
   )
 }
 
