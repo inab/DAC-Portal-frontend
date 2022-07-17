e8d0ef07e75ec783344e9e5e8eae0b03df766a50
acavalls <alejandro.canosa@bsc.es>
2022-07-17 11:21:30 +0200

    Server -> User requests (PUT - only DACs): Adding Mongoose Transaction (ACID) for handling Data Access Requests status (DAC-Portal) and permissions (Permissions-API)

--- server/services/dacmember.js
+++ server/services/dacmember.js
@@ -1,38 +1,105 @@
 // IMPORT MONGOOSE MODELS.
-import { DacRequests } from '../models/dac-requests';
-import { DacData } from '../models/dac-data';
-
-/* FUNCTIONS */
+import mongoose from 'mongoose';
+import { DacRequests } from '../models/DAC-Portal/dac-requests';
+import { DacData } from '../models/DAC-Portal/dac-data';
+import { UserPermissions } from '../models/Permissions-API/user';
 
 /* 1. MONGO DB: */
 
-// 1.a. GET USER REQUESTS ASSIGNED TO THIS DAC-MEMBER BY ID -> EVENTUALLY, THIS QUERY WILL USE DAC-ID INSTEAD (WE DON'T HAVE DAC IDs YET) 
+// 1.A. ACCEPT REQUEST TRANSACTION: DAC-PORTAL-DB: STATUS -> ACCEPTED, PERMISSIONS-API: ADD DATA PERMISSIONS. 
+const acceptRequestTransaction = async (adminDacs, userId, resource) => {
+    const session = await mongoose.startSession();
 
+    let isCompleted = false;
+
+    try {
+        session.startTransaction();
+        const firstResponse = await Promise.all(adminDacs.map(async (item) => {
+            return await acceptRequestStatus(item.dacId, userId, resource, session)
+        }))
+
+        if(firstResponse === null) throw new Error("Status not updated")
+
+        const secondResponse = await createUserPermissions(userId, resource, session)
+
+        if(secondResponse === null) throw new Error("Permissions not updated")
+
+        await session.commitTransaction();
+    } catch (e) {
+        console.log("error", e)
+        await session.abortTransaction();
+        return { response: false };
+    } finally {
+        session.transaction.state === "TRANSACTION_COMMITTED" ? isCompleted = true : isCompleted = false
+        await session.endSession();
+        return { response: isCompleted };
+    }
+}
+
+// 1.A.1. ACCEPT REQUEST TRANSACTION: DAC-PORTAL-DB: STATUS -> ACCEPTED
+const acceptRequestStatus = async (id, userId, resource, session) => {
+    let response = await DacRequests.findOneAndUpdate(
+        { 'dacId': id, 'requests.user': userId, 'requests.requests.resource': resource },
+        {  $set : { "requests.$[i].requests.$[j].status" : "Accepted" }},
+        {  arrayFilters: [{ "i.user": userId }, { "j.resource": resource }], new: true, session }
+    );
+
+    return response
+}
+
+// 1.A.2. ACCEPT REQUEST TRANSACTION: PERMISSIONS-API: ADD DATA PERMISSIONS
+const createUserPermissions = async (id, resource, session) => {
+    const assertion = {
+        type: "ControlledAccessGrants",
+        asserted: "1564814387",
+        value: resource,
+        source: "https://test-url/source_dac",
+        by: "dac",
+    }
+    const user = await UserPermissions.updateOne(  { 'sub' : id },
+                                { $setOnInsert: { "assertions" : assertion } },
+                                { new: true, upsert: true, session })                                       
+
+    let response = await UserPermissions.findOneAndUpdate(
+                                    { 'sub' : id, 'assertions.value' : resource },
+                                    { $set : { "assertions.$" : assertion } },
+                                    { new: true, session });  
+    
+    if(!response) {
+        await UserPermissions.findOneAndUpdate(
+            { 'sub' : id }, 
+            { $addToSet : { "assertions" : assertion } },
+            { new: true, session });
+
+        response = await UserPermissions.findOne({'sub': id, "assertions.value": resource}, null, { session })
+    }
+
+    return response
+}
+
+// 1.B. GET USER REQUESTS ASSIGNED TO THIS DAC-MEMBER BY ID
 const getUserRequests = async (id) => {
-    const response = await DacRequests.find({ 'dacId': id })
+    const response = await DacRequests.find({ 'dacId': { $in: id } })
                                       .select({ 'requests': 1, 'dacId': 1, '_id': 0 });
 
     return response
 }
 
-// 1.a. GET USER REQUESTS ASSIGNED TO THIS DAC-MEMBER BY ID
-
+// 1.C. GET USER REQUESTS ASSIGNED TO THIS DAC-MEMBER BY ID
 const getUserDacs = async (id) => {
     const response = await DacData.find({ 'members': id })
                                   .select({ '_id': 0, 'members': 0, 'files': 0 });
     return response
 }
 
-// 1.b. GET DAC DATA: DAC Data associated to a specific user.
-
+// 1.D. GET DAC DATA: DAC Data associated to a specific user.
 const getDacData = async (id) => {
     const response = await DacData.find({ 'members': id })
                                   .select({ '_id': 0, 'members': 0 });
     return response
 }
 
-// 1.c. Update policy: DAC Data.
-
+// 1.E. UPDATE POLICIES: DAC Data.
 const updatePolicies = async (id, dac, file, policy) => {
 
     let resourceData = await DacData.find({ 'dacId': dac, 'files.fileId': file })
@@ -52,8 +119,7 @@ const updatePolicies = async (id, dac, file, policy) => {
     return response
 }
 
-// 1.d. GET POLICIES BY DAC-ID AND FILE-ID
-
+// 1.F. GET POLICIES BY DAC-ID AND FILE-ID
 const getPolicies = async (dac, file) => {
     const response = await DacData.find({ 'dacId': dac, 'files.fileId': file })
                                   .select({ '_id': 0, 'members': 0, 'files.$': 1 });
@@ -61,8 +127,7 @@ const getPolicies = async (dac, file) => {
     return response
 }
 
-// 1.e. Update DAC info: 
-
+// 1.G. UPDATE DAC INFO: 
 const updateDacInfo = async (id, info) => {
 
     let { dacId,
@@ -92,4 +157,11 @@ const updateDacInfo = async (id, info) => {
     return response
 }
 
-export { getUserRequests, getDacData, getPolicies, getUserDacs, updatePolicies, updateDacInfo }
+export { 
+    getUserRequests, 
+    getDacData, 
+    getPolicies, 
+    getUserDacs, 
+    updatePolicies, 
+    updateDacInfo, 
+    acceptRequestTransaction }
