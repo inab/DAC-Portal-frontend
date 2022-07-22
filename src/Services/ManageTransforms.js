b703aeb40dfa4ef4ca778b658c85c845332e223f
acavalls <alejandro.canosa@bsc.es>
2022-07-22 07:45:40 +0200

    Refactoring (IV.a): Even more abstraction -> Completely replacing old Transforms (funct. inheritance + pipes) with Monads

--- src/Services/ManageTransforms.js
+++ src/Services/ManageTransforms.js
@@ -1,55 +1,3 @@
-const TransformPipeline = (data) => {
-
-    let that = {}
-
-    that.requestObjects, that.resources, that.users, that.status = [];
-    that.setRequestObjects = () => that.requestObjects = data.flatMap(item => item.requests)
-    that.pipe = () => (...fns) => fns.reduce((a, b) => (x) => b(a(x)));
-
-    return that;
-};
-
-const TransformPipelineToAccepted = (data) => {
-
-    let that = TransformPipeline(data)
-
-    that.setStatus = () => that.status = that.requestObjects.flatMap(item => item.status === "Accepted" ? true : false)
-    that.setUserIds = () => that.users = data.map(item => item.user)
-    that.setResourcesIds = () => that.resources = that.requestObjects.flatMap(item => item.resource)
-
-    that.pipe(
-        that.setRequestObjects(),
-        that.setUserIds(),
-        that.setResourcesIds(),
-        that.setStatus())
-
-    const getAcceptedUsers = () => [...new Set(that.users.filter((user, i) => that.status[i]))]
-    const getAcceptedResources = () => [...new Set(that.resources.filter((resource, i) => that.status[i]))]
-
-    return { getAcceptedUsers, getAcceptedResources }
-}
-
-const TransformPipelineToPending = (data) => {
-
-    let that = TransformPipeline(data)
-
-    that.setStatus = () => that.status = that.requestObjects.flatMap(item => item.status === "Pending" ? true : false)
-    that.setUserObjects = () => that.users = data.map((el) => Object.assign({}, { ['user']: el.user }))
-    that.setPendingUsers = () => that.users = [...that.users.filter((user, i) => that.status[i])]
-    that.setPendingRequests = () => that.requests = [...that.requestObjects.filter((req, i) => that.status[i])]
-
-    that.pipe(
-        that.setRequestObjects(),
-        that.setUserObjects(),
-        that.setStatus(),
-        that.setPendingUsers(),
-        that.setPendingRequests())
-
-    const getUsersAndRequests = () => that.users.map((user, index) => Object.assign(user, that.requests[index]))
-
-    return { getUsersAndRequests }
-}
-
 const Monad = (value) => {
     // MONAD => FUNCTOR (MAP) + FLAT (FLATMAP)
     const map = (fn) => Monad(fn(value));
@@ -59,4 +7,20 @@ const Monad = (value) => {
     return { map, flatMap, current }
 }
 
-export { Monad, TransformPipelineToAccepted, TransformPipelineToPending }
+const setObjects = (data) => data
+    .flatMap(item => item.requests
+    .map(request => Object.assign({}, { ['user']: item.user, ...request })))
+
+const selectRequests = (data) => data.flatMap(item => item.requests)
+
+const filterPending = (data) => data.filter(item => item.status === "Pending")
+
+const filterAccepted = (data) => data.filter(item => item.status === "Accepted") 
+
+const getRequests = (data) => Monad(data).map(selectRequests).flatMap(selectRequests)
+
+const getPendingRequests = (data) => Monad(data).map(setObjects).flatMap(filterPending)
+
+const getAcceptedRequests = (data) => Monad(data).map(setObjects).flatMap(filterAccepted)
+
+export { getRequests, getPendingRequests, getAcceptedRequests }
