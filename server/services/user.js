616bcb68df2569a08a60dc886884357b0dda7112
acavalls <alejandro.canosa@bsc.es>
2022-07-28 09:55:41 +0200

    Server -> Services (user): Adapting methods to the new data model + removing unnecessary transaction

--- server/services/user.js
+++ server/services/user.js
@@ -1,6 +1,4 @@
 // IMPORT MONGOOSE MODELS.
-import mongoose from 'mongoose';
-import { User } from '../models/DAC-Portal/user';
 import { DacRequests } from '../models/DAC-Portal/dac-requests';
 import { DacData } from '../models/DAC-Portal/dac-data';
 
@@ -33,8 +31,9 @@ const getRequestedFileData = async (file) => {
 }
 
 // 1.C BUILD REQUEST OBJECT FROM FILE INFORMATION AND USER REQUEST METADATA
-const buildRequestObject = (file, resource, comments) => {
+const buildRequestObject = (id, file, resource, comments) => {
     return {
+        user: id,
         fileId: file,
         resource: resource,
         comment: comments,
@@ -42,102 +41,42 @@ const buildRequestObject = (file, resource, comments) => {
     }
 }
 
-// 1.D CREATE A TRANSACTION FOR STORING USER REQUEST DATA (collections: user (USER-VIEW) - dac-requests (DAC VIEWS))
-const requestTransaction = async (id, dacId, request) => {
-    const session = await mongoose.startSession();
-
-    let isCompleted = false;
-
-    try {
-        session.startTransaction();
-        await setUserRequestData(id, dacId, request, session)
-        await setUserRequestStatus(id, request, session)
-        await session.commitTransaction();
-    } catch (e) {
-        await session.abortTransaction();
-        return { response: false };
-    } finally {
-        if (session.transaction.state === "TRANSACTION_COMMITTED") {
-            isCompleted = true;
-        } else {
-            isCompleted = false;
-        }
-        await session.endSession();
-        return { response: isCompleted };
-    }
-}
-
-
 // 1.E SET USER REQUEST DATA (DAC VIEW)
-const setUserRequestData = async (id, dacId, request, session) => {
+const setUserRequestData = async (dacId, request) => {
 
-    let userReq = { 'user': id, 'requests': [request] }
-
-    let dacReq = { 'dacId' : dacId, 'requests' : [userReq] }
+    let dacReq = { 'dacId' : dacId, 'requests' : [request] }
 
     let dac =  await DacRequests.find({ 'dacId' : dacId })
-                                .select({'_id' : 0 });
-    
-    let user = await DacRequests.find({ 'dacId' : dacId, 'requests.user' : id })
                                 .select({'_id' : 0 });
 
     let response = [];
 
     if(dac.length === 0) {
-        response = await DacRequests.create([{ 'dacId' : dacReq.dacId, 'requests' : dacReq.requests }], { session });
-    } else if(user.length === 0) {
-        response = await DacRequests.findOneAndUpdate(
-            { 'dacId' : dacId },
-            { $addToSet : { 'requests' : userReq } },
-            { new: true, session });
+        response = await DacRequests.create([{ 'dacId' : dacReq.dacId, 'requests' : dacReq.requests }]);
     } else {
         response = await DacRequests.findOneAndUpdate(
-            { 'dacId' : dacId, 'requests.user' : id },
-            { $addToSet : { "requests.$.requests" : request } },
-            { new: true, session }); 
+            { 'dacId' : dacId },
+            { $addToSet : { "requests" : request } },
+            { new: true }); 
     } 
 
     return response
 }
 
-// 1.F SET USER REQUEST STATUS (USER VIEW)
-const setUserRequestStatus = async (id, request, session) => {
-    let fileStatusObject = {
-        fileId: request.fileId,
-        status: "Pending"
-    }
-    let user = await User.find({ 'sub' : id })
-                         .select({'_id' : 0 });
-
-    let response;
-
-    if(user.length === 0){
-        response = await User.create([{ 'sub': id, 'status': fileStatusObject }], { session });
-    } else {
-        response = await User.findOneAndUpdate(
-            { 'sub': id },
-            { $addToSet: { "status" : fileStatusObject }},
-            { new: true, session }
-        )
-    }
-
-    return response
-}
-
-// 1.G. GET REQUESTS STATUS BY USER ID
-/*const getRequestsStatus = async (id) => {
-    const response = await User.find({ 'sub' : id })
-                               .select({ 'status' : 1, '_id' : 0});
-    return response
-}*/
-
-// 1.H. GET USER REQUESTS ASSIGNED BY USERID
+// 1.H. GET REQUESTS BY USER ID
 const getRequestsStatus = async (id) => {
-    const response = await DacRequests.find({ 'requests.user': id })
-                                      .select({ '_id': 0, 'requests.requests.$': 1 });
+    const response = await DacRequests.aggregate([
+        { $unwind: "$requests" },
+        { $match: { 'requests.user': id } },
+        { $project: {  
+            fileId: '$requests.fileId', 
+            resource: '$requests.resource', 
+            comment: '$requests.comment',
+            status: '$requests.status' }
+        }
+    ])
 
     return response
 }
 
-
-export { getPolicies, getRequestsStatus, getRequestedFileData, buildRequestObject, requestTransaction }
+export { getPolicies, getRequestsStatus, getRequestedFileData, buildRequestObject, setUserRequestData }
