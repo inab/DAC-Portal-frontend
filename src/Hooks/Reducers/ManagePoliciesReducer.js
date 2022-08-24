d64988e2faf40c33bb81a82e04655f68de8e24d6
acavalls <alejandro.canosa@bsc.es>
2022-08-24 11:50:06 +0200

    Client -> Refactoring (VI.d): Creating multiple reducers for managing different parts of the state

--- src/Hooks/Reducers/ManagePoliciesReducer.js
+++ src/Hooks/Reducers/ManagePoliciesReducer.js
@@ -2,36 +2,60 @@ import { useCallback, useReducer } from 'react';
 
 const { REACT_APP_DAC_PORTAL_API_URL } = process.env
 
-const INITIAL_STATE = {
+const INITIAL_STATE_REQUESTS = {
     type: 'get',
     url: `${REACT_APP_DAC_PORTAL_API_URL}/dac/policies`,
     token: localStorage.getItem("react-token"),
-    params: null
+    params: null,
+    index: null,
+    value: null
 }
 
-const policiesReducer = (state, action) => {
+const INITIAL_STATE_INPUT = {
+    index: null,
+    value: null
+}
+
+const policiesRequestReducer = (state, action) => {
     switch (action.type) {
         case "get":
             return INITIAL_STATE
-        case "put": 
+        case "put": {
             const { object } = action.payload;
             return {
                 ...state,
                 type: 'put',
                 url: `${REACT_APP_DAC_PORTAL_API_URL}/dac/policies`,
-                params: { 
-                    'dac-id' : `${object.dacId}`,
-                    'ds-id' : `${object.fileId}`,
-                    'acl' : `${object.acl}`,
+                params: {
+                    'dac-id': `${object.dacId}`,
+                    'ds-id': `${object.fileId}`,
+                    'acl': `${object.acl}`,
                     'policy': `${object.policy}`
                 }
             }
+        }      
+    }
+}
+
+const policiesInputReducer = (state, action) => {
+    switch (action.type) {
+        case "change": {
+            const { evt } = action.payload;
+            return {
+                ...state,
+                index: evt.target.getAttribute('data-id'),
+                value: evt.target.value
+            }
+        }        
     }
 }
 
 const useRequest = (object) => {
-    const [request, dispatch] = useReducer(policiesReducer, INITIAL_STATE)
+    const [request, dispatch] = useReducer(policiesRequestReducer, INITIAL_STATE_REQUESTS)
 
+    console.log("state: request")
+    console.log(request)
+    
     const handlers = useCallback(({
         putItem: (object) => {
             dispatch({ type: "put", payload: { object: object } })
@@ -40,4 +64,18 @@ const useRequest = (object) => {
     return [request, handlers]
 }
 
-export default useRequest
+const useInput = (evt) => {
+    const [input, dispatch] = useReducer(policiesInputReducer, INITIAL_STATE_INPUT)
+
+    console.log("state: input")
+    console.log(input)
+
+    const handlers = useCallback(({
+        changeItem: (evt) => {
+            dispatch({ type: "change", payload: { evt: evt } })
+        }
+    }))
+    return [input, handlers]
+}
+
+export { useRequest, useInput }
