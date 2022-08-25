674ae2f31cfbc01f421bbecd1391b03618a8f539
acavalls <alejandro.canosa@bsc.es>
2022-08-25 10:26:55 +0200

    Client -> Component (Table): Adding input & children extension (row)

--- src/components/Table/TableRow.tsx
+++ src/components/Table/TableRow.tsx
@@ -2,24 +2,34 @@ import React from 'react';
 import { Button } from "react-bootstrap";
 import { ITableRowProps, ITableRowPropsWithButtons } from '../../Models/Table';
 
-const DisplayRow: React.FC<ITableRowProps> = ({ row, exclude, children }) => {
+const DisplayRow: React.FC<ITableRowProps> = ({ row, index, changePolicy, exclude, children }) => {
     const filters = exclude ? exclude : [""]
     const filteredRow = Object.entries(row).filter((keyValuePair) => !filters.includes(keyValuePair[0]))
+
     return (
-        <> 
-            {filteredRow.map((keyValuePair) => <td> {keyValuePair[1]} </td>)} 
-            {children} 
+        <>
+            {filteredRow.map((keyValuePair) => keyValuePair[0] === 'policy' ? 
+                <td className="text-center">
+                    <input data-id={index} type="text" value={keyValuePair[1]} onChange={changePolicy} />
+                </td> : <td> {keyValuePair[1]} </td>
+            )}
+            {children}
         </>
     )
 }
 
-const DisplayRowWithButtons: React.FC<ITableRowPropsWithButtons> = ({ row, exclude, putItem, deleteItem }) => {
+const DisplayRowWithButtons: React.FC<ITableRowPropsWithButtons> = ({ row, index, exclude, putItem, deleteItem, savePolicy, changePolicy }) => {
     return (
-        <DisplayRow row={row} exclude={exclude}>
-            <td className="text-center">
-                <Button variant="success" className="btn-block btn-fill" onClick={putItem}>Grant</Button>
-                <Button variant="danger" className="btn-block btn-fill" onClick={deleteItem}>Deny</Button>
-            </td>
+        <DisplayRow row={row} index={index} exclude={exclude} changePolicy={changePolicy}>
+            { !savePolicy ?
+                <td className="text-center">
+                    <Button variant="success" className="btn-block btn-fill" onClick={putItem}>Grant</Button>
+                    <Button variant="danger" className="btn-block btn-fill" onClick={deleteItem}>Deny</Button>
+                </td> : 
+                <td className="text-center">
+                    <Button variant="success" className="btn-block btn-fill" onClick={savePolicy}>Update</Button>
+                </td>            
+            }
         </DisplayRow>
     )
 }
