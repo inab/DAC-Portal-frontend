d1e4ff8cde9e068c82688e89bf725db0c03f1bb5
acavalls <alejandro.canosa@bsc.es>
2022-08-31 18:43:21 +0200

    Client -> Reducers (ManagePermissions): Adding .tsx + types & Ifaces

--- src/Hooks/Reducers/ManagePermissionsReducer.tsx
+++ src/Hooks/Reducers/ManagePermissionsReducer.tsx
@@ -1,43 +1,51 @@
-import { useCallback, useReducer } from 'react';
+import { useReducer } from 'react';
+import { State, Actions, Dispatchers } from '../../Models/ManagePermissionsReducer';
 
 const { REACT_APP_DAC_PORTAL_API_URL, REACT_APP_PERMISSIONS_URL } = process.env
 
-const INITIAL_STATE = {
+const INITIAL_STATE : State = {
     type: 'get',
     url: `${REACT_APP_DAC_PORTAL_API_URL}/dac/requests/accepted`,
     token: localStorage.getItem("react-token"),
     params: {
-      'format': null,
-      'account-id': null
-    }
+        'format': undefined,
+        'account-id': undefined,
+        'acl': undefined,
+        'object-id': undefined,
+        'values': undefined
+    },
+    index: undefined
 }
 
-const permissionsReducer = (state, action) => {
+const permissionsReducer = (state: State, action: Actions): any => {
     switch (action.type) {
         case "delete":
             const { object, index } = action.payload;
+
             return {
                 ...state,
                 type: 'delete',
                 url: `${REACT_APP_PERMISSIONS_URL}/permissions`,
-                params: { 
-                    'values': `${object.value}`,
-                    'account-id': `${object.sub}`
+                params: {
+                    'account-id': object.sub,
+                    'values': object.value
                 },
                 index: index
             }
+        default:
+            return state
     }
 }
- 
-const useRequest = (object, index) => {
+
+const useRequest = () => {
     const [request, dispatch] = useReducer(permissionsReducer, INITIAL_STATE)
-  
-    const handlers = useCallback(({
+ 
+    const handlers: Dispatchers = {
         deleteItem: (object, index) => {
             dispatch({ type: "delete", payload: { object: object, index: index } })
         }
-    }))
+    }
     return [request, handlers]
 }
 
-export default useRequest
+export { useRequest }
