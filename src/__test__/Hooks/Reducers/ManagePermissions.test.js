247ff2fe831bf177bfb6da3dfc910b26cf0f16ae
acavalls <alejandro.canosa@bsc.es>
2022-09-12 17:19:28 +0200

    Tests (unit): Testing reducer (ManagePermissions)

---
+++ src/__test__/Hooks/Reducers/ManagePermissions.test.js
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom/extend-expect';
+import { act, renderHook } from '@testing-library/react-hooks';
+import { useRequest } from '../../../Hooks/Reducers/ManagePermissionsReducer';
+//import { prettyDOM } from '@testing-library/dom'
+
+const mockRequest = {
+    sub: "testUID",
+    value: "testDatasetID",
+    index: 0
+}
+
+test('Permissions reducer updates the state. type=delete ', () => {
+    const { result } = renderHook(() => useRequest())
+
+    const [state, { deleteItem }] = result.current
+
+    act(() => {
+        deleteItem(mockRequest, mockRequest.index)
+    }) 
+
+    expect(result.current[0].params.values).toBe("testDatasetID")
+
+})
