fa563aac13ba6be31e9dc6ff81ad4a3acca158e2
acavalls <alejandro.canosa@bsc.es>
2022-09-19 18:04:38 +0200

    Server: AuthZ Middleware -> Adding a new condition for validation

--- server/middleware/authZ.js
+++ server/middleware/authZ.js
@@ -12,7 +12,6 @@ export default async (req, res, next) => {
 	if(!dacScopes) throw createError(403, "Forbidden");
 	// Check if user has the dac-admin role (delete).
 	const isDacAdmin = checkRole(dacScopes.roles, "dac-admin");
-
 	// B. ABAC: Check if the DAC has access to the resources.
 	let isControlledResource;
 
@@ -20,7 +19,7 @@ export default async (req, res, next) => {
         if(!dacScopes) throw createError(403, "Forbidden");
     }
 
-	if(req.method === "PUT" && isDacAdmin && req.path !== "/info") {
+	if(req.method === "PUT" && isDacAdmin && (req.path !== "/info" || req.path !== "/requests/deny")) {
 		isControlledResource = checkResource(dacScopes.resources, req.param('acl'));
 		if(!isControlledResource) throw createError(403, "Forbidden");
 	}
