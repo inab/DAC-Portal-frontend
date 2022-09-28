f99ca5695fb85b644c9a9e0d4fc5eef9b0a85bd7
acavalls <alejandro.canosa@bsc.es>
2022-09-28 18:07:18 +0200

    Removing framework/library components from outside the infrastructure layer

--- src/Routes.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { RouteComponentProps } from "react-router-dom";
-import { Routes } from "./Domain/Entities/Entities"
-
-export type RouteWithFC = Routes & {
-    component: React.FC<RouteComponentProps>
-}
