b089201c0e5ad1de38b260de150c1cfb715553f5
acavalls <alejandro.canosa@bsc.es>
2023-01-03 11:32:41 +0100

    Application layer: Updating CreateDAC use cases

--- src/Application/UseCases/CreateDAC.ts
+++ src/Application/UseCases/CreateDAC.ts
@@ -3,22 +3,14 @@ import { DACRequestParams } from '../../Domain/Ports/Requests';
 import { requestsController } from '../Controllers/Requests';
 import { http } from '../../Infrastructure/Adapters/Http';
 
-const getUserDACs = async (request : DACRequestParams) => {
+const getUserDACs = async (request : DACRequestParams) : Promise<Array<DACInfo>> => {
     const data = await requestsController(http).getUserDACs(request)
-    return data.length > 0 ? parseUserDacs(data) : []
+    return data.length > 0 ? data : []
 }
 
 const updateDACInfo = async (request : DACRequestParams) => {
     const data = await requestsController(http).updateDACInfo(request)
-    return data.length > 0 ? parseUserDacs(data) : []
-}
-
-const parseUserDacs = (dacs: Array<DACInfo>) => {
-    const parsed = dacs.map((item: any) => {
-        const { dacId, info } = item
-        return Object.assign({}, { dacId: dacId, ...info })
-    })
-    return parsed
+    return [data]
 }
 
 export { getUserDACs, updateDACInfo }
